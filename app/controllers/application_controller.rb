0ae3c40b8ff6e979287fe50e3351f062c94af95d
david30-maker <davidokpah396@gmail.com>
2023-07-25 12:56:39 +0100

    ADD:implement linter setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def index
-    render json: { message: "Welcome to my new Rails app!" }
+    render json: { message: 'Welcome to my new Rails app!' }
   end
 end
